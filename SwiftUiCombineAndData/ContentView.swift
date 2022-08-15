441ebd31f6db38ae9936f3bfdbc5a22dbb298c82
ahafizi <a.hafizov@team.alif.tj>
2022-08-15 18:45:09 +0500

    Initial Commit

---
+++ SwiftUiCombineAndData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiCombineAndData
+//
+//  Created by Abdullo Hafizov on 15/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiCombineAndData/SwiftUiCombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCombineAndDataApp.swift
+//  SwiftUiCombineAndData
+//
+//  Created by Abdullo Hafizov on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
